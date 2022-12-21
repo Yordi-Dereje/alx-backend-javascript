67388c21e448f951620f10f537f604a7a1ecc592
Yordi-Dereje <yordadereje@gmail.com>
2022-12-22 01:23:46 +0300

    1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,8 @@
-export default function getFullResponseFromAPI(success) {
+export default function getResponseFromAPI(success) {
   const p = new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ status: 200, body: 'Success', });
-    }
-    else {
-      reject(Error('The fake API is not working currently'));
-    }
+    if (success) resolve({ status: 200, body: 'Success' });
+    else reject(Error('The fake API is not working currently'));
   });
+
   return p;
 }
