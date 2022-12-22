76017f09fd9cd912e49b521b3c22361e2fcb8bb6
Yordi-Dereje <yordadereje@gmail.com>
2022-12-22 22:41:33 +0300

    3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -6,10 +6,12 @@ export default class Currency {
 
   // getters
   get code() { return this._code; }
+  
   get name() { return this._name; }
 
   // setters
   set code(code) { this._code = code; }
+  
   set name(name) { this._name = name; }
 
   displayFullCurrency() {
