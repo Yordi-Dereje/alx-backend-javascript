1bcafaea7b407152522e874f6c8e641465572e6a
Yordi-Dereje <yordadereje@gmail.com>
2023-02-22 13:26:46 +0300

    4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,14 +1,20 @@
-/**
-* Create a small HTTP server using the http module.
-*/
 const http = require('http');
 
-const port = 1245;
+const PORT = 1245;
+const HOST = 'localhost';
+const app = http.createServer();
 
-const app = http.createServer((req, res) => {
- res.end('Hello Holberton School!');
+app.on('request', (_, res) => {
+  const responseText = 'Hello Holberton School!';
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
 });
 
-app.listen(port);
+app.listen(PORT, HOST, () => {
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
+});
 
 module.exports = app;
